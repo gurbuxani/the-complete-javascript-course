27dc10cbb641d055a0d4b0e05d70ba3491f1dc1d
gurbuxani <gurbuxani@users.noreply.github.com>
2018-01-17 19:26:55 +0530

    Progress till lecture 42 + UI changes

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -13,19 +13,48 @@ var score, roundScore, activePlayer, dice;
 
 score = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none'; // Hide dice before the game starts
-
 
 document.querySelector('.btn-roll').addEventListener('click', function() { // When the player clicks the roll dice button
   diceValue = Math.floor(Math.random() * 6) + 1; // Picks a random number between 1 and 6 and stores it to var diceValue
   var diceDOM = document.querySelector('.dice'); //Shorthand for the dice's document selector property
   diceDOM.style.display = 'block'; // Displays the dice
   diceDOM.src = 'dice-' + diceValue + '.png' // Displays the correct .png file for the dice, based on random diceValue
+  
+  if (diceValue !== 1) {
+    roundScore += diceValue;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore; //Display score for current dice roll
+  } else {
+    nextPlayer();
+  }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  score[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+  
+  if (score[activePlayer] >= 10) {
+    document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+    document.querySelector('.dice').src = 'winner-' + activePlayer + '.png';
+    document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+    document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
+  } else {
+  nextPlayer();  
+  }
+});
 
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+}
 
-// document.querySelector('#current-' + activePlayer).textContent = dice; //Display score for current dice roll
+// 
 // var x = document.querySelector('#score-' + activePlayer).textContent //Store score for current dice roll to var x
